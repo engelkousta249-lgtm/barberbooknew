a2eb5d1be4fe5ccd982e570dc51c08d0e4d9b91b
engelkousta249 <engelkousta249@gmail.com>
2026-07-21 22:35:39 +0300

    update page

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -2,7 +2,8 @@ import Stripe from "stripe"
 import { NextResponse } from "next/server"
 import { createClient } from "@supabase/supabase-js"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+const stripe = new Stripe("sk_test_51TuaQsK25ZaTzOJhEyXttqGHnYltCStZlNohradGqVrNHZ4RuyvDM6TuRFgpmb6ZLl4nDR7GVfNjyAMnzXw2horY00oFfzV5Yu")
+
 const supabase = createClient(
   "https://xcfkhdjiragblsiqetes.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjZmtoZGppcmFnYmxzaXFldGVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2OTY0ODYsImV4cCI6MjA5NzI3MjQ4Nn0.EkmgRuYzrvF0A_pgT9vaOouMRKeQ2kasPZxpoIuCgeE"
